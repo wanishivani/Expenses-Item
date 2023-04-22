a3b12e48a35b38a23b209c37e389d6e721f43f84
wanishivani <127400106+wanishivani@users.noreply.github.com>
2023-04-22 13:44:01 +0530

    Add files via upload

--- Expensesitem.js
+++ Expensesitem.js
@@ -1,23 +1,33 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
-import React from 'react';
+import React,{useState} from 'react';
 
 function Expensesitem(props) {
+  const [tittle,setTittle]=useState(props.tittle,props.amount);
+  const [amount]=useState(props.amount);
+
+ console.log('Expensesitem evaluated bt React');
   const clickHandler = () =>{
-    console.log("clicked!!!")
+
+   setTittle('Updated!!');
+    console.log(tittle);
+    setTittle('Added!!');
+    console.log(amount);
+
+
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div>{props.date.toLocaleString()}</div>
-      <h2>{props.tittle}</h2>
+      <h2>{tittle}</h2>
 
       <div className="expense-items_descreption">
-        <div className="Expense-item_price">${props.amount}</div>
+        <div className="Expense-item_price">${amount}</div>
       </div>
       <button onClick={clickHandler}>Change tittle</button>
-
+      <button onClick={clickHandler}>Add</button>
     </Card>
   );
 }
