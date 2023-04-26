eafb867cfd9ce94dd92e27273eceb17f85989bb1
wanishivani <127400106+wanishivani@users.noreply.github.com>
2023-04-26 13:23:32 +0530

    Add files via upload

--- Expense.js
+++ Expense.js
@@ -18,26 +18,15 @@ return(
     
     <Card className='expense'>
       <ExpenseFillter selected={Fillteryear} onChangeFilter={FilterChangeHandler}/>
-      <Expensesitem
-        tittle={props.item[0].tittle}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
+      {props.item.map((expense)=>(
+       <Expensesitem
+       key={expense.id}
+      tittle={expense.tittle}
+      amount={expense.amount}
+      date={expense.date}
       />
-      <Expensesitem
-        tittle={props.item[1].tittle}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      />
-      <Expensesitem
-        tittle={props.item[2].tittle}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      />
-      <Expensesitem
-        tittle={props.item[3].tittle}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      />
+      )
+      )};
     </Card>
   );
 }
