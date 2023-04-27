e9634830878742ab20f80b7266d9d30ade471f90
wanishivani <127400106+wanishivani@users.noreply.github.com>
2023-04-27 23:36:40 +0530

    Add files via upload

--- Expensesitem.js
+++ Expensesitem.js
@@ -10,9 +10,7 @@ function Expensesitem(props) {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div>{props.date.toLocaleString()}</div>
       <h2>{props.tittle}</h2>
-
       <div className="expense-items_descreption">
         <div className="Expense-item_price">${props.amount}</div>
       </div>
